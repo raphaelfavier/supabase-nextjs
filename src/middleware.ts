2ab93e271c28d71201fb2024870842cdfe8fddbc
Raph <1252156+raphaelfavier@users.noreply.github.com>
2025-06-17 16:26:52 +0200

    chore: add comments to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,11 +12,13 @@ export async function middleware(request: NextRequest) {
   const requestedPath = request.nextUrl.pathname;
 
   if (!user) {
+    // user must be logged in to access any page under /profile
     if (requestedPath.startsWith("/profile")) {
       return NextResponse.redirect(new URL("/auth/login", request.url));
     }
   } else {
     if (
+      // logged user does not have access to login and signup pages
       requestedPath.startsWith("/auth/login") ||
       requestedPath.startsWith("/auth/signup")
     ) {
